c78a9ec0d9dc555382d5522b743d80cf31c55596
Jan Drgona <jan.drgona@pnnl.gov>
2020-12-14 16:03:00 -0800

    small update

--- tests/test_all_systems.py
+++ tests/test_all_systems.py
@@ -14,22 +14,20 @@ if __name__ == '__main__':
             ninit = 0
             building = psl.BuildingEnvelope()  # instantiate building class
             building.parameters(system='HollandschHuys_full', linear=False)  # load model parameters
-            # simulate open loop building
-            out = building.simulate(ninit=ninit)
-            # plot trajectories
-            psl.plot.pltOL(Y=out['Y'], U=out['U'], D=out['D'], X=out['X'])
-            psl.plot.pltPhase(X=out['Y'])
+            out = building.simulate(ninit=ninit)  # simulate open loop
+            psl.plot.pltOL(Y=out['Y'], U=out['U'], D=out['D'], X=out['X'], figname="./figs/"+name+"_ol")
+            psl.plot.pltPhase(X=out['Y'], figname="./figs/"+name+"_phase")
             plt.close('all')
         elif isinstance(system(), psl.ODE_NonAutonomous):
             model = system(nsim=12000)
             out = model.simulate()  # simulate open loop
-            psl.plot.pltOL(Y=out['Y'], U=out['U'])  # plot trajectories
-            psl.plot.pltPhase(X=out['Y'])
+            psl.plot.pltOL(Y=out['Y'], U=out['U'], figname="./figs/"+name+"_ol")  # plot trajectories
+            psl.plot.pltPhase(X=out['Y'], figname="./figs/"+name+"_phase")
             plt.close('all')
         elif isinstance(system(), psl.ODE_Autonomous):
             model = system()
             out = model.simulate()  # simulate open loop
-            psl.plot.pltOL(Y=out['Y'])  # plot trajectories
-            psl.plot.pltPhase(X=out['Y'])
+            psl.plot.pltOL(Y=out['Y'], figname="./figs/"+name+"_ol")  # plot trajectories
+            psl.plot.pltPhase(X=out['Y'], figname="./figs/"+name+"_phase")
             plt.close('all')
 
