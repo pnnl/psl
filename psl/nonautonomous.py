ec6eaf35f2a273fea23a2ce2a7d48bd77ebdf95b
Shaw Cortez <shaw199@we29705.pnl.gov>
2022-02-21 21:33:45 -0700

    updated Iver_kin_reduced model with correct model and barrier to avoid singularities.

--- psl/nonautonomous.py
+++ psl/nonautonomous.py
@@ -645,16 +645,17 @@ class Iver_kin_reduced(ODE_NonAutonomous):
         self.nx = 5    # Number of states
         self.nu = 5    # Number of control inputs
         self.ts = 0.1
+        self.K_alpha = 100.0 # Barrier function parameter
 
         # Initial Conditions for the States
-        self.x0 = np.array([1.0, 0, 0, 0, 0])
+        self.x0 = np.array([0, 0, 0, 0, 0])
 
         seed = 3
         u = SplineSignal(nsim=self.nsim, values=None, xmin=1.0, xmax=3.0, rseed=seed)
-        v = SplineSignal(nsim=self.nsim, values=None, xmin=-0.1, xmax=0.1, rseed=seed)
-        w = SplineSignal(nsim=self.nsim, values=None, xmin=-0.1, xmax=0.1, rseed=seed)
-        q = SplineSignal(nsim=self.nsim, values=None, xmin=-0.01, xmax=0.01, rseed=seed)
-        r = SplineSignal(nsim=self.nsim, values=None, xmin=-0.01, xmax=0.01, rseed=seed)
+        v = SplineSignal(nsim=self.nsim, values=None, xmin=-0.5, xmax=0.5, rseed=2*seed)
+        w = SplineSignal(nsim=self.nsim, values=None, xmin=-0.5, xmax=0.5, rseed=3*seed)
+        q = SplineSignal(nsim=self.nsim, values=None, xmin=-0.5, xmax=0.5, rseed=4*seed)
+        r = SplineSignal(nsim=self.nsim, values=None, xmin=-0.5, xmax=0.5, rseed=5*seed)
 
         self.U = np.vstack( [u, v, w, q, r] ).T
 
@@ -666,13 +667,30 @@ class Iver_kin_reduced(ODE_NonAutonomous):
         + Inputs (5): [u, v, w, q, r]
         """
 
+        # States
+        xi = x[0]
+        eta = x[1]
+        zeta = x[2]
+        theta = x[3]
+        psi = x[4]
+
+        # Control
+        uu = u[0]
+        v = u[1]
+        w = u[2]
+        q = u[3]
+        r = u[4]
+
+        # Barrier function to avoid singularities
+        h = (np.pi/2.0 - 0.01)**2 - theta**2
+        z = -2.0*theta*q + self.K_alpha*h
 
         dx_dt = np.zeros(5)
-        dx_dt[0] = np.cos( x[4] )*np.cos( x[3] )*u[0] - np.sin( x[4] )*u[1] + np.sin( x[3] )*np.cos( x[4] )*u[2]
-        dx_dt[1] = ( np.sin( x[4] )*np.cos( x[3] )*np.cos( x[4] )*np.sin( x[3] )*np.sin( x[4] ) - np.sin( x[3] ) )*u[0]
-        dx_dt[2] = np.cos( x[3] )*u[2]
-        dx_dt[3] = u[3]
-        dx_dt[4] = u[4]/(np.cos( x[3] ))
+        dx_dt[0] = np.cos( psi )*np.cos( theta )*uu - np.sin( psi )*v + np.sin( theta )*np.cos( psi )*w
+        dx_dt[1] =  np.sin( psi )*np.cos( theta )*uu + np.cos( psi )*v + ( np.sin( theta )*np.sin( psi ) )*w
+        dx_dt[2] = -np.sin( theta )*uu + np.cos( theta )*w
+        dx_dt[3] = q - z
+        dx_dt[4] = r/(np.cos( theta ))
 
         return dx_dt
 
