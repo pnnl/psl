464582a28801f224cace1cd1fe8c0c11a678f25e
Christian Møldrup Legaard <christian.legaard@pnnl.gov>
2022-06-23 13:58:24 -0700

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -5,24 +5,29 @@ requirements = [
 ]
 
 setup(
-    name='psl',
+    name="psl",
     description="dynamic Systems Library in Python",
     license="MIT",
     author="Aaron Tuor, Jan Drgona, Elliott Skomski, Soumya Vasisht",
-    author_email='aarontuor@gmail.com',
-    url='https://github.com/aarontuor/psl',
-    packages=['psl'],
-    package_data={'psl': ['psl/parameters/buildings/*']},
-    entry_points={
-        'console_scripts': [
-            'psl=psl.cli:cli'
-        ]
-    },
+    author_email="aarontuor@gmail.com",
+    url="https://gitlab.pnnl.gov/dadaist/psl",
+    packages=["psl"],
+    package_data={"psl": ["psl/parameters/buildings/*"]},
+    entry_points={"console_scripts": ["psl=psl.cli:cli"]},
     install_requires=requirements,
-    keywords='psl',
+    keywords="psl",
     classifiers=[
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ], include_package_data=True
+        "Programming Language :: Python :: 3.10",
+    ],
+    include_package_data=True,
+    python_requires=">=3.10",
+    install_requires=[
+        "gym",
+        "matplotlib",
+        # "neuromancer" currently imported by a single script causing a circular dependency between psl and neuromancer
+        "numpy",
+        "pandas",
+        "pyts",
+        "scipy",
+    ],
 )
