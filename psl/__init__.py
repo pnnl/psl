9e1f167dba9df0dcf14fc01ac00f95e851ab4fc0
tuor369 <aaron.tuor@pnnl.gov>
2022-06-28 10:07:02 -0700

    changing name of datasets to data and removing friction stir welding stuff

--- psl/__init__.py
+++ psl/__init__.py
@@ -2,5 +2,15 @@ import psl.autonomous as auto
 import psl.nonautonomous as nauto
 import psl.ssm as ssm
 import psl.emulator as emulator
+import psl.plot as plot
+
+from psl.datasets import datasets
+
+# from psl.autonomous import *
+# from psl.nonautonomous import *
+# from psl.ssm import *
+# from psl.emulator import *
+from psl.perturb import Periodic
 
 systems = {**auto.systems, **nauto.systems, **ssm.systems, **emulator.systems}
+emulators = systems
