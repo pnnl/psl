60fe10c47b21815073c21972eb0a02d8bf7c01f9
tuor369 <aaron.tuor@pnnl.gov>
2022-08-17 12:50:14 -0700

    removed install_requires

--- setup.py
+++ setup.py
@@ -10,16 +10,15 @@ setup(
     packages=["psl"],
     package_data={"psl": ["psl/parameters/buildings/*"]},
     entry_points={"console_scripts": ["psl=psl.cli:cli"]},
-    python_requires=">=3.9",
-    install_requires=[
-        "gym",
-        "matplotlib",
-        # "neuromancer" currently imported by a single script causing a circular dependency between psl and neuromancer
-        "numpy",
-        "pandas",
-        "pyts",
-        "scipy",
-    ],
+    # install_requires=[
+    #     "gym",
+    #     "matplotlib",
+    #     # "neuromancer" currently imported by a single script causing a circular dependency between psl and neuromancer
+    #     "numpy",
+    #     "pandas",
+    #     "pyts",
+    #     "scipy",
+    # ],
     keywords="psl",
     classifiers=[
         "Programming Language :: Python :: 3.10",
